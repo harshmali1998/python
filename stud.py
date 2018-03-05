1f51d7565032d2698cb06f0eadc0cf804c392f13
harshmali1998 <37060056+harshmali1998@users.noreply.github.com>
2018-03-05 11:59:34 +0530

    Create stud.py

---
+++ stud.py
@@ -0,0 +1,27 @@
+    def personalinfo():
+        name= input("Enter Your name = ")
+        Rollno = int(input("Enter Your Roll Number = "))
+        cls = input("Enter Your Class = ")
+        div = input("Enter Your Division  = ")
+    def putinfo():
+        print(" Name = ",name)
+        print(" Roll num = ",Rollno)
+        print(" Class = ",cls)
+        print(" Division = ",div)
+    def getmks():
+            sub1 = int(input("Enter Marks In First Subject = "))
+            sub2 = int(input("Enter Marks In Second Subject = "))
+            sub3 = int(input("Enter Marks In Third Subject = "))
+            print(" Average of marks is = ",(sub1+sub2+sub3)/3)
+
+#main
+
+print("****************************Student Information********************************")
+print(" \n\t 1. Enter Information \n\t 2. Print Information \n\t 3. Exit ")
+print("\n\t")
+ch = int(input("Enetr your Choice = "))
+if(ch ==
+personalinfo()
+getmks()
+            
+        
